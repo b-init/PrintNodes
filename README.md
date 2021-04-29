dc3f2b94f19bd6cf51a264b5309a0d6624f013fc
Binit <binitnew@gmail.com>
2021-04-29 20:53:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+**PrintNodes**
+==
+
+Blender addon to take high quality screenshots of node trees irrespective of the display resolution.
+Works with blender 2.80 and above.
+
+__*Please read the Usage and Notes section before using the addon*__
+
+Installation
+--
+* Download the latest release from [releases page on github](https://github.com/Yeetus3141)
+* Go to `Edit > Preferences > Addons > Install` and select the downloaded zip file.
+* Make sure that `Secondary Directory` is set as per your wish.
+
+Usage
+--
+* In the Node Editor, you __*MUST*__ go to fullscreen area mode via `Ctrl+Alt+Space` or `View > Area > Toggle Fullscreen Area` in the header before using the addon.
+* The scroll bars must not be visibly at any time while the process takes place. You can ensure that by making sure the cursor stays around the middle of the screen.
+* Then, make sure you're zoomed to a level where you cover most area while the text is still readable, to prevent oversized screenshots (in resolution).
+* Now from the Node Context menu `(Right Click)`, start the screenshot process. It'll take a little while to take the shot. DO NOT perform any action until you get a finishing popup message.
+* But you can force-cancel the process mid-way by pressing `Esc` or `Rightmouse Button`.
+
+Notes
+--
+* Can take shots of whole node tree, within node groups, or only the selected nodes (using the `Take Screenshot Of Selected Nodes` option.)
+* It is recommended to save the blend file before using the addon.
+* Selecting `NodeFrames` while using `Take Screenshot Of Selected Nodes` Feature will crash the application. Keep that in mind.
+
+
+Changelog
+--
+**v1.0.0 (28th Apr, 21)** 
+	* Initial release. 
+
+
+Additional Info
+--
+For any questions, suggestions or bug reports, [join my discord server](https://discord.gg/G8ajxwQuYT) contact me via twitter **@YeetusBlenditus** or e-mail me at **binitnew@gmail.com**
---
+++ __init__.py
@@ -0,0 +1,297 @@
+#PrintNodes addon for Blender 2.80+ to take high quality screenshots of node trees
+#Managed by: Binit (aka Yeetus)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+bl_info = {
+    "name" : "PrintNodes",
+    "author" : "Binit",
+    "description" : "Takes high quality screenshot of a node tree",
+    "blender" : (2, 80, 0),
+    "version" : (1, 0, 0),
+    "location" : "Node Editor > Context Menu (Right Click)",
+    "warning" : "",
+    "category" : "Node"
+}
+
+import bpy
+from bpy.types import Operator, AddonPreferences, Menu
+from bpy.props import StringProperty, BoolProperty, IntProperty
+
+import time
+import os
+from .PIL import Image, ImageChops
+
+
+def MakeDirectory(): # Manage Directory for saving screenshots
+
+    if bpy.data.filepath and bpy.context.preferences.addons[__name__].preferences.force_secondary_dir == False: 
+        # save image in the place where the blendfile is saved, in a newly created subfolder (if saved and force_default_directory is set to false)
+        Directory = os.path.join(os.path.split(bpy.data.filepath)[0], 'NodesShots')
+        
+        if os.path.isdir(Directory) == False:
+            os.mkdir(Directory)
+            bpy.ops.wm
+    else:  
+        # just use the secondary directory otherwise
+        Directory = bpy.context.preferences.addons[__name__].preferences.secondary_save_dir
+
+    return Directory
+
+
+class PRTND_PT_Preferences(AddonPreferences): # setting up perferences
+    bl_idname = __name__
+
+    secondary_save_dir: StringProperty(
+        name = "Secondary Directory",
+        subtype = 'DIR_PATH',
+        default = bpy.context.preferences.filepaths.temporary_directory,
+        )
+
+    force_secondary_dir: BoolProperty(
+        name = "Always Use Secondary Directory",
+        default = False,
+        )
+
+    padding_amount: IntProperty(
+        name = "Padding Amount (in px)",
+        default = 30,
+        )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text = "A subfolder in the same directory as the blend file will be used to save the images.")
+        layout.label(text = "Unless the file is unsaved or 'Always Use Secondary Directory' is checked.")
+        layout.label(text = "In whcich case, the Secondary Directory will be used")
+        layout.prop(self, "secondary_save_dir")
+        layout.prop(self, "force_secondary_dir")
+        layout.separator()
+        layout.prop(self, "padding_amount")
+
+
+class PRTND_MT_ContextMenu(Menu): 
+    """Context Menu For Print Nodes"""
+    bl_idname = "PRTND_MT_context_menu"
+    bl_label = "PrintNodes"
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(PRTND_OT_ModalScreenshotTimer.bl_idname, text = "Take Screenshot Of Whole Tree", icon = "NODETREE")
+        layout.operator(PRTND_OT_ModalScreenshotTimer.bl_idname, text = "Take Screenshot Of Selected Nodes", icon = "SELECT_SET").selection_only = True
+
+
+def PrintNodesPopUp(message = "", title = "PrintNodes PopUp", icon = ""): # function to display popup message on command
+
+    def draw(self, context):
+        self.layout.label(text = message)
+
+    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+
+
+class PRTND_OT_ModalScreenshotTimer(Operator): # modal operator to take parts of the whole shot every at every set interval, while not interrupting the rest of blender's functioning (for the most part)
+    """Take screenshot of active node tree. Press RightClick or Esc to cancel during process."""
+    bl_idname = "prtnd.modal_ss_timer"
+    bl_label = "Take Tree Screenshot"
+
+    selection_only: BoolProperty(default = False)
+
+    _timer = None
+    Xmin = Ymin = Xmax = Ymax = 0
+    ix = iy = 0
+    temp_name = ''
+    temp_grid_level = 0
+    forced_cancel = False
+
+
+    def modal(self, context, event): 
+        if event.type in {'RIGHTMOUSE', 'ESC'}: # force cancel
+            self.forced_cancel = True
+            self.cancel(context)
+            return {'CANCELLED'}
+
+        if event.type == 'TIMER':
+                  
+            tree = context.space_data.edit_tree
+            view = context.region.view2d
+            area = bpy.context.area
+            dx = area.width - 1
+            dy = area.height - 1
+            
+            path = os.path.join(MakeDirectory(), f'Prt_y{self.iy}_x{self.ix}.png')
+            bpy.ops.screen.screenshot(filepath=path, full = False) # take screenshot of current view as a 'tile' to be further stitched and processed
+
+            if tree.view_center[1] > self.Ymax and tree.view_center[0] > self.Xmax: # check if already at the other corner of the tree, if yes, sucessfully terminate
+                self.cancel(context)
+                return {'CANCELLED'}
+            
+            if tree.view_center[0] > self.Xmax: # if exceeded rightmost edge, pan all the way back to leftmost edge and pan y up once to prepare for the next 'layer' of tiles
+                bpy.ops.view2d.pan(deltax = -(self.ix*dx), deltay=dy)
+                self.ix = 0
+                self.iy += 1
+
+            else: # just pan to the right if no other condition applies (i.e. we're somewhere in the middle of the tile strip)
+                bpy.ops.view2d.pan(deltax = dx, deltay = 0)
+                self.ix += 1
+
+        return {'PASS_THROUGH'} # pass for next iteration
+
+    def execute(self, context):
+        
+        invisible_ascii_char = '⠀' # magically invisible ascii character for super secret reasons (which is, so that the material label on the screen doesn't appear and mess up the shot)
+
+        self.temp_name = context.space_data.id.name
+        context.space_data.id.name = invisible_ascii_char
+
+        self.temp_grid_level = context.preferences.themes[0].node_editor.grid_levels
+        bpy.context.preferences.themes[0].node_editor.grid_levels = 0 # turn gridlines off, trimming empty space doesn't work otherwise
+
+        if self.selection_only:
+            nodes = context.selected_nodes # perform within the selected nodes only
+        else:  
+            nodes = context.space_data.edit_tree.nodes # perform within the whole tree
+        tree = context.space_data.edit_tree
+
+        self.Xmin = self.Xmax = nodes[0].location[0]
+        self.Ymin = self.Ymax = nodes[0].location[1]
+    
+
+        for i in range(len(nodes)):
+            loc = nodes[i].location
+            locX = loc[0]
+            locY = loc[1]
+            
+            if locX < self.Xmin:
+                self.Xmin = locX
+            if locY < self.Ymin:
+                self.Ymin = locY
+
+            if locX > self.Xmax:
+                self.Xmax = locX
+            if locY > self.Ymax:
+                self.Ymax = locY
+            
+        node = tree.nodes.new("NodeReroute") # new reroute node as guide for the algorithm to work
+        node.location = self.Xmin, self.Ymin
+
+        for current_node in nodes:
+            current_node.select = False
+        
+        node.select = True
+        bpy.ops.wm.redraw_timer(iterations=1) # Force Updates 2dview so that the ndoe location gets updated. this one ugly line makes the whole addone uncompatible for being an official addon, may find a workaround later. But gotta live with it for now
+        bpy.ops.node.view_selected() # align view to the (bottom-left) corner node. As an initial point for the screenshotting process
+        
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.02, window=context.window) # add timer to begin with, for the `modal` process
+        wm.modal_handler_add(self)
+
+        return {'RUNNING_MODAL'}
+
+    def cancel(self, context):
+
+        if self.forced_cancel: 
+            PrintNodesPopUp(message = "Process Force Cancelled", icon = "CANCEL")
+
+        else:
+            area = bpy.context.area
+            StitchTiles(area.width, area.height, self.ix + 1, self.iy + 1) # being the stitching and processing process of the tiles
+            PrintNodesPopUp(message = "Screenshot Saved Successfully", icon = "CHECKMARK")
+
+        # revert all the temporary settings back to original
+        context.preferences.themes[0].node_editor.grid_levels = self.temp_grid_level
+        context.space_data.id.name = self.temp_name
+
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+
+
+def TrimImage(img): # function to trim out empty space from the edges, leaving a padding (as defined in preferences)
+
+    bg_clr = tuple(bpy.context.preferences.themes[0].node_editor.space.back * 255) # get the background color of the shader editor from themes and map from 0-1 to 0-255 for PIL operations
+    bg_clr = tuple(map(lambda i: int(i), bg_clr)) # convert float tuple to int tuple (as Image.new(color) expectes Int tuple)
+
+    padding = bpy.context.preferences.addons[__name__].preferences.padding_amount
+    padding_tuple = (-padding, -padding, padding, padding) # to subtract padding amount from x_min, y_min and add to x_max, y_max for cropping
+    
+    img_w, img_h = img.size
+    img_size_tuple = (img_w, img_h, img_w, img_h) # tuple of image size in format (x_min, y_min, x_max, y_max) for clamping padding amount, with some 'hacky' elements
+
+
+    bg = Image.new(img.mode, img.size, bg_clr)
+    diff = ImageChops.difference(img, bg)
+    bbox = diff.getbbox()
+
+    crop_tuple = tuple(map(lambda i, j: i + j, bbox, padding_tuple)) # offset the co-ords to leave space for padding
+    crop_tuple = tuple(map(lambda i, j: max(0, min(i, j)), crop_tuple, img_size_tuple)) # clamp all values to not be outside the image (negative or greater than size of image)
+
+    if bbox:
+        return img.crop(crop_tuple)
+    else:
+        return img
+
+
+def StitchTiles(tile_width, tile_height, num_x, num_y): # function to stitch multiple tiles to one single image
+
+    folder_path = MakeDirectory()
+
+    out_canvas = Image.new('RGB', (tile_width * num_x, tile_height * num_y))
+
+    for y in range(num_y):
+        for x in range(num_x):
+            tile_path = os.path.join(folder_path, f'Prt_y{y}_x{x}.png')
+            current_tile = Image.open(tile_path)
+            out_canvas.paste(current_tile, (tile_width * x, tile_height * (num_y - (y + 1))))
+            os.remove(tile_path) #remove used tiles
+
+    out_canvas = TrimImage(out_canvas)
+
+    timestamp = time.strftime("%y%m%d-%H%M%S")
+    out_path = os.path.join(folder_path, f'NodeTreeShot{timestamp}.png')
+    out_canvas.save(out_path)
+
+
+
+# menu function(s)
+def PrintNodes_menu_func(self, context):
+    self.layout.menu(PRTND_MT_ContextMenu.bl_idname, icon="FCURVE_SNAPSHOT")
+
+classes = (PRTND_OT_ModalScreenshotTimer, PRTND_PT_Preferences, PRTND_MT_ContextMenu, )
+
+#addon_keymaps = []
+
+
+def register():
+
+    for current in classes:
+        bpy.utils.register_class(current)
+
+    bpy.types.NODE_MT_context_menu.append(PrintNodes_menu_func)
+
+    # wm = bpy.context.window_manager
+    # kc = wm.keyconfigs.addon
+    # if kc:
+    #     km = wm.keyconfigs.addon.keymaps.new(name='Node Editor', space_type='NODE_EDITOR')
+    #     kmi = km.keymap_items.new(PRTND_OT_ModalScreenshotTimer.bl_idname, 'C', 'PRESS', ctrl=True, shift=True)
+    #     addon_keymaps.append((km, kmi))
+
+def unregister():
+
+    for current in classes:
+        bpy.utils.unregister_class(current)
+
+    bpy.types.NODE_MT_context_menu.remove(PrintNodes_menu_func)
+
+    # for km, kmi in addon_keymaps:
+    #     km.keymap_items.remove(kmi)
+    # addon_keymaps.clear()
