c9bb3aa33f4e0c92e1440ac23b5e7d4e528bd591
Binit <binitnew@gmail.com>
2022-11-25 18:33:29 +0530

    Bump to v1.2.0

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "author" : "Binit",
     "description" : "Takes high quality screenshot of a node tree",
     "blender" : (3, 1, 0),
-    "version" : (1, 1, 8),
+    "version" : (1, 2, 0),
     "location" : "Node Editor > Context Menu (Right Click)",
     "warning" : "",
     "category" : "Node"
